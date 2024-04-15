b76cbdf2f9d0d17e1f849f27c1b75a6f8075eda1
Couragous77 <couragetikum@gmail.com>
2024-04-15 13:34:24 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, DirectionalLight } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+const loader = new GLTFLoader();
+
+loader.load( 'scene.gltf', function ( gltf ) {
+
+	const root = gltf.scene;
+	
+root.scale.set(0.5, 0.5, 0.5)
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(2,2,5);
+scene.add(light);
+
+camera.position.z = 60;
+
+function animate() {
+	requestAnimationFrame( animate );
+cube.rotation.x += 0.01;
+cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+animate();
